40c9a8ca25d02ae1b2bc9ecd0a886b477c889845
Shreyas Ramakrishna <31136074+Shreyasramakrishna90@users.noreply.github.com>
2019-09-03 14:07:40 -0500

    update

--- Module-2-MiddlewareAndBackend/examples/ZMQ/pub-sub/subscriber.py
+++ Module-2-MiddlewareAndBackend/examples/ZMQ/pub-sub/subscriber.py
@@ -6,10 +6,10 @@ def main():
 
         context = zmq.Context()
         socket= context.socket(zmq.SUB)
-        socket.connect("tcp://localhost:5200")#SUB has to subscribe to messages
+        socket.connect("tcp://127.0.0.1:5200")#SUB has to subscribe to messages
         socket.setsockopt(zmq.SUBSCRIBE, b'')#This is a filter to receive all incoming messages
         while True:
-        	msg=socket.recv()#receive messages
+        	msg=socket.recv_string()#receive messages
         	print(msg)
         socket.close()
 
