7402efebae876b194a9d61ac3d29293222ab958b
Shreyas Ramakrishna <31136074+Shreyasramakrishna90@users.noreply.github.com>
2019-09-03 14:09:58 -0500

    Update server.py

--- Module-2-MiddlewareAndBackend/examples/MQTT/server.py
+++ Module-2-MiddlewareAndBackend/examples/MQTT/server.py
@@ -1,30 +1,36 @@
-#Server.py script which subscribes to a topic being published by the client
-#Here we use free online MQTT brokers
-#Try any of the three available online brokers: test.mosquitto.org, broker.hivemq.com, iot.eclipse.org
-#To locally install MQTT broker use: sudo apt-get install mosquitto
-#This creates a broker on your localhost (127.0.0.1)
-#Use mosquitto -v to see the message communication between the client and server
-
+#!/usr/bin/python3
+from time import time
 import datetime
+#from datetime import datetime
+import random
 import time
+import csv
+import os
+import zmq
 import paho.mqtt.client as mqtt
+broker= "127.0.0.1"#"192.168.99.100" #"192.168.1.184"#"test.mosquitto.org"
+port=2000
+topic = "code"
 
 def on_message(client, userdata, message):
     print(message.payload.decode('utf-8'))
 
+def on_connect(client, userdata, flags, rc):
+    print('connected')
+    client.subscribe(topic)
+
 def main():
-    broker= "iot.eclipse.org"#"127.0.0.1"
-    topic="MQTT"
-    mqtt.Client.connected_flag=False
-    client = mqtt.Client("Server")#create an instance of client
-    client.on_message= on_message
-    client.loop_start()#start the loop
+    mqtt.Client.connected_flag=False#create flag in class
+    broker="127.0.0.1"#"test.mosquitto.org"
+    client = mqtt.Client("Server")             #create new instance
+    client.on_message= on_message                      #attach function to callback
+    client.on_connect = on_connect
+   #bind call back function
+    client.loop_start()
     print("Connecting to broker ",broker)
-    client.connect(broker)#connect to broker
+    client.connect(broker)
     while True:
-        client.subscribe("MQTT")#subscribe to a required topic
-    client.loop_stop()#stop the loop
-    client.disconnect()#disconnect from the broker
+        pass
 
 if __name__=="__main__":
 	main()
