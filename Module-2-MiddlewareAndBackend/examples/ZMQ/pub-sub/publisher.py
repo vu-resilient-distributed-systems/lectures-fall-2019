9a96706188ca9237ce6f10973de1dd8f838e9802
Shreyas Ramakrishna <31136074+Shreyasramakrishna90@users.noreply.github.com>
2019-09-04 10:58:01 -0500

    update publisher

--- Module-2-MiddlewareAndBackend/examples/ZMQ/pub-sub/publisher.py
+++ Module-2-MiddlewareAndBackend/examples/ZMQ/pub-sub/publisher.py
@@ -11,13 +11,20 @@ def main():
     context = zmq.Context()
     socket= context.socket(zmq.PUB)
     socket.bind("tcp://127.0.0.1:5200")#PUB has to bind to an address & port
-    while True:
-        for i in range (0,100):
-            msg = randint(1,100)#publishing random numbers every 2s
-            socket.send_string(str(msg))#Send messages
-            print(msg)
-            time.sleep(2)
-    socket.close()
+    print("I want to publish messages")
+    try:
+        while True:
+            for i in range (0,100):
+                msg = randint(1,100)#publishing random numbers every 2s
+                socket.send_string(str(msg))#Send messages
+                print(msg)
+                time.sleep(2)
+    except KeyboardInterrupt:
+        print("W: interrupt received, stopping…")
+    finally:
+        socket.close()
+        context.term()
 
 if __name__=="__main__":
 	main()
+
