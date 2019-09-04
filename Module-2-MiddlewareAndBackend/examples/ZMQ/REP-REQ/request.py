81aef8854328c643694bed9e91f0cb284c6ad7af
Shreyas Ramakrishna <31136074+Shreyasramakrishna90@users.noreply.github.com>
2019-09-04 10:59:23 -0500

    Update and rename client.py to request.py

---
+++ Module-2-MiddlewareAndBackend/examples/ZMQ/REP-REQ/request.py
@@ -0,0 +1,28 @@
+#Simple script to request from the server
+#Uses the REP-REQ messaging pattern
+#Simplest Hello-World to get a feel of REP-REQ
+
+import zmq
+import time
+
+def main():
+
+    context = zmq.Context()
+    socket= context.socket(zmq.REQ)#Request socket
+    socket.connect("tcp://localhost:5200")#connect to the socket
+    print("Will request a message")
+    try:
+        while True:
+            socket.send_string("Hi Can you send me the code")#send a request message
+            msg=socket.recv_string()#Receive a reply back
+            print(msg)
+            time.sleep(2)
+    except KeyboardInterrupt:
+        print("W: interrupt received, stopping…")
+    finally:
+        socket.close()#close sockets
+        context.term()#terminate the context
+
+
+if __name__=="__main__":
+	main()
