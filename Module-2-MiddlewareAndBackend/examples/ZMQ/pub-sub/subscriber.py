66bc4c8c82df7db69abf147ad1097aeaf9291ae5
Shreyas Ramakrishna <31136074+Shreyasramakrishna90@users.noreply.github.com>
2019-09-04 10:58:27 -0500

    update subscriber

--- Module-2-MiddlewareAndBackend/examples/ZMQ/pub-sub/subscriber.py
+++ Module-2-MiddlewareAndBackend/examples/ZMQ/pub-sub/subscriber.py
@@ -8,11 +8,16 @@ def main():
         socket= context.socket(zmq.SUB)
         socket.connect("tcp://127.0.0.1:5200")#SUB has to subscribe to messages
         socket.setsockopt(zmq.SUBSCRIBE, b'')#This is a filter to receive all incoming messages
-        while True:
-        	msg=socket.recv_string()#receive messages
-        	print(msg)
-        socket.close()
-
+        print("I want to subscribe to messages sent by the publisher")
+        try:
+            while True:
+                msg=socket.recv_string()#receive messages
+                print(msg)
+        except KeyboardInterrupt:
+            print("W: interrupt received, stopping…")
+        finally:
+            socket.close()
+            context.term()
 
 if __name__=="__main__":
 	main()
