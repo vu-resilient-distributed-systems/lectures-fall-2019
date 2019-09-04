6b8354912dc0def47669839b75f1d33f8989fe31
Shreyas Ramakrishna <31136074+Shreyasramakrishna90@users.noreply.github.com>
2019-09-04 11:02:36 -0500

    Update and rename client.py to publisher.py

--- Module-2-MiddlewareAndBackend/examples/MQTT/publisher.py
+++ Module-2-MiddlewareAndBackend/examples/MQTT/publisher.py
@@ -1,16 +1,14 @@
-from time import time
-import datetime
-#from datetime import datetime
 import random
 import time
-import csv
 import os
 import zmq
 import paho.mqtt.client as mqtt
-broker= "127.0.0.1"#"192.168.99.100" #"192.168.1.184""test.mosquitto.org"
-port=2000
-topic = "code"
+broker= "127.0.0.1"#local broker on your host machine
+#"192.168.99.100" #"192.168.1.184""test.mosquitto.org" #Are available online brokers
+port = 2000
+topic1 = "RDS19"#Topic1 to use for the communication
 
+#on connect check if you are still conected to the broker
 def on_connect(client, userdata, flags, rc):
     if rc==0:
         client.connected_flag=True #set flag
@@ -20,23 +18,20 @@ def on_connect(client, userdata, flags, rc):
 
 def main():
     mqtt.Client.connected_flag=False#create flag in class
-    broker="127.0.0.1"#"test.mosquitto.org"
-    client = mqtt.Client("client1")             #create new instance
+    client = mqtt.Client("client1")#create new instance
     client.on_connect=on_connect  #bind call back function
-    client.loop_start()
+    client.loop_start()#start the loop
     print("Connecting to broker ",broker)
-    client.connect(broker)
+    client.connect(broker)#connect to the broker
     while not client.connected_flag: #wait in loop
         print("In wait loop")
         time.sleep(1)
     print("Main Loop")
-    msg="hello"
     while True:
-        client.publish(topic, msg)
-        print(msg)
+        client.publish(topic1,str(random.randint(0,100)))#keep publishing in the loop
+        print("message published")
         time.sleep(2)
-
-    client.disconnect()
+    client.disconnect()#Disconnect when done
 
 if __name__=="__main__":
 	main()
